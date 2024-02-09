edb16f173fe4e9583f46d21d45a006cfa8e80d64
Kailas Puri <kailaspuri123@gmail.com>
2024-02-09 12:58:41 +0530

    first commit

---
+++ data.js
@@ -0,0 +1,12 @@
+// module.exports = () => {
+//   const data = {
+//     teachers: [],
+//   };
+//   for (let i = 0; i < 100; i++) {
+//     data.teachers.push({
+//       id: i,
+//       title: `Teacher ${i}`, // Change title format for better readability
+//     });
+//   }
+//   return data;
+// };
